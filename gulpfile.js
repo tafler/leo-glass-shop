0896b503726f442cee3b105435a262318c783cc3
tafler <taffik@yandex.ru>
2018-04-17 16:14:26 +0300

    commit with gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() { 
 	gulp.src("./scss/style.scss")   //откуда брать файлы
 	.pipe(sass().on('error', sass.logError))//поток(.не фаталит а пишет в лог)
-	.pipe(gulp.dest('./css/'));	//куда складывать
-	})
+	.pipe(gulp.dest('./css/'))	//куда складывать
+	.pipe(reload({stream: true}));
+	});
 
 
-gulp.task('sass:watch', function() {
-	gulp.watch("./scss/**/*.scss", ['sass']); //("какие файлы смотреть", "какой таск запускать")
-	})
+gulp.task('sass:watch', ['sass', 'browser-sync'] , function() {
+	gulp.watch("./scss/**/*.scss", ['sass']);//("какие файлы смотреть", "какой таск запускать")
+
+})
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: "./" 
+		}
+	});
+});
